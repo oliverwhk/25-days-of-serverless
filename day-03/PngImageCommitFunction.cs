86dadcc232a24f4fead97b0a77b1840bd1257e1f
Oliver <oliverwhk@gmail.com>
2019-12-20 22:57:10 +1300

    Day 3 challenge - Updated azure function.

--- day-03/PngImageCommitFunction.cs
+++ day-03/PngImageCommitFunction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,31 +21,46 @@ namespace day_03
             // Validate the GitHub Event Type
             if (!req.Headers.ContainsKey("X-GitHub-Event") || req.Headers["X-GitHub-Event"] != "push")
             {
+                log.LogInformation("Skipped because request is not a GitHub push event.");
                 return new BadRequestObjectResult("Incorrect webhook trigger");
             }
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var gitCommitEvents = JsonConvert.DeserializeObject<GitCommitEvent>(requestBody);
 
+            var pngImageFilePaths = GetPngImageFilePaths(log, gitCommitEvents);
+            PersistPngImageFilePaths(log, pngImageFilePaths);
+
+            return new OkObjectResult("");
+        }
+
+        private static IEnumerable<string> GetPngImageFilePaths(ILogger log, GitCommitEvent gitCommitEvents)
+        {
             foreach (var commit in gitCommitEvents.Commits)
             {
                 foreach (var added in commit.Added)
                 {
-                    log.LogInformation($"Added file path: {gitCommitEvents.Repository.Url}/raw/master/{added}");
-                    // if (added.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
-                    // {
-                    //     
-                    // }
+                    if (added.EndsWith(".png", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var pngFilePath = $"{gitCommitEvents.Repository.Url}/raw/master/{added}";
+
+                        log.LogInformation($"Extracted PNG file path: {pngFilePath}");
+
+                        yield return pngFilePath;
+                    }
                 }
             }
+        }
 
-            //TODO: Extract PNG urls from the event object
-            log.LogInformation("TODO: Extract PNG urls from the event object");
+        private static void PersistPngImageFilePaths(ILogger log, IEnumerable<string> pngImageFilePaths)
+        {
+            if (pngImageFilePaths.Count() == 0)
+            {
+                log.LogInformation("No PNG image file paths to processed.");
+                return;
+            }
 
-            //TODO: Persist down to azure table storage
             log.LogInformation("TODO: Persist down to azure table storage");
-
-            return new OkObjectResult("");
         }
     }
 }
