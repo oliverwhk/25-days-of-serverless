9df1bd6e00f95a2c3fb8ef063db83788c082c22d
Oliver Wong <oliver.wong@serko.com>
2020-01-03 14:13:33 +1300

    Day 4 challenge - Updated dish model.

--- day-04/Model/Dish.cs
+++ day-04/Model/Dish.cs
@@ -1,7 +1,15 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace day_04.Model
 {
     public class Dish
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
         public string Name { get; set; }
     }
 }
