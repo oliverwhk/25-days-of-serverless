168ff27c810ae35cc4f6e673864f3290895696a1
Oliver Wong <oliver.wong@serko.com>
2020-01-03 14:51:40 +1300

    Day 4 challenge - Added create dish endpoint (WIP).

--- day-04/DishesFunction.cs
+++ day-04/DishesFunction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using day_04.Model;
 using Microsoft.AspNetCore.Mvc;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using Newtonsoft.Json;
 
 namespace day_04
 {
@@ -24,17 +26,34 @@ namespace day_04
             return new OkObjectResult(dishes);
         }
         
+        // [FunctionName("GetDishById")]
+        // public static async Task<IActionResult> GetDishById([HttpTrigger(AuthorizationLevel.Function, "get", Route = "dishes")] HttpRequest req, ILogger log)
+        // {
+        //     var client = new MongoClient(System.Environment.GetEnvironmentVariable("MongoDbAtlasConnectionString"));
+        //     var database = client.GetDatabase(System.Environment.GetEnvironmentVariable("MongoDbName"));
+        //     var collection = database.GetCollection<Dish>("dishes");
+        //     var dishes = await collection.Find(new BsonDocument()).ToListAsync();
+        //
+        //     return new OkObjectResult(dishes);
+        // }
+        
         [FunctionName("CreateDish")]
         public static async Task<IActionResult> CreateDishes([HttpTrigger(AuthorizationLevel.Function, "post", Route = "dishes")] HttpRequest req, ILogger log)
         {
+            // using var streamReader = new StreamReader();
+            // var requestBody = await streamReader.ReadToEndAsync();
+            // var model = JsonConvert.DeserializeObject<Dish>(requestBody);
+
             var client = new MongoClient(System.Environment.GetEnvironmentVariable("MongoDbAtlasConnectionString"));
             var database = client.GetDatabase(System.Environment.GetEnvironmentVariable("MongoDbName"));
             var collection = database.GetCollection<Dish>("dishes");
 
+
+
             var newDish = new Dish { Name = $"new dish {DateTime.Now}" };
             await collection.InsertOneAsync(newDish);
 
-            return new OkObjectResult("");
+            return new OkObjectResult(newDish);
         }
     }
 }
