a54cba00033e48163144729a87b3dc52279cc30a
Oliver Wong <oliver.wong@serko.com>
2020-01-17 09:06:41 +1300

    Day 6 challenge - Tidied up durable function.

--- day-06/Scheduler.cs
+++ day-06/Scheduler.cs
@@ -7,38 +7,39 @@ using Microsoft.Extensions.Logging;
 
 namespace day_06
 {
-    public static class DurableOrchestrator
+    public static class Scheduler
     {
-        [FunctionName("DurableOrchestrator")]
+        [FunctionName(nameof(Scheduler))]
         public static async Task<List<string>> RunOrchestrator(
             [OrchestrationTrigger] DurableOrchestrationContext context)
         {
             var outputs = new List<string>();
 
             // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("DurableOrchestrator_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("DurableOrchestrator_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("DurableOrchestrator_Hello", "London"));
+            var sayHelloFunctionName = nameof(Scheduler) + "_" + nameof(SayHello);
+            outputs.Add(await context.CallActivityAsync<string>(sayHelloFunctionName, "Tokyo"));
+            outputs.Add(await context.CallActivityAsync<string>(sayHelloFunctionName, "Seattle"));
+            outputs.Add(await context.CallActivityAsync<string>(sayHelloFunctionName, "London"));
 
             // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
             return outputs;
         }
 
-        [FunctionName("DurableOrchestrator_Hello")]
+        [FunctionName(nameof(Scheduler) + "_" + nameof(SayHello))]
         public static string SayHello([ActivityTrigger] string name, ILogger log)
         {
             log.LogInformation($"Saying hello to {name}.");
             return $"Hello {name}!";
         }
 
-        [FunctionName("DurableOrchestrator_HttpStart")]
+        [FunctionName(nameof(Scheduler) + "_" + nameof(HttpStart))]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "schedule")]HttpRequestMessage req,
             [OrchestrationClient]DurableOrchestrationClient starter,
             ILogger log)
         {
             // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("DurableOrchestrator", null);
+            string instanceId = await starter.StartNewAsync(nameof(Scheduler), null);
 
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
